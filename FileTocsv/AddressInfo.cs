d5a8b968da7dff62f57faa99ea41cfdd42d9990b
Sagar Lad <sagar.lad@morningstar.com>
2021-08-25 08:26:35 +0530

    Added data from file to CSV

---
+++ FileTocsv/AddressInfo.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using CsvHelper.Configuration;
+
+public class AddressInfoClassMap : ClassMap<AddressInfo>
+{
+    public AddressInfoClassMap()
+    {
+        Map(r => r.Name).Name("Name");
+        Map(r => r.Organisation).Name("organisation");
+        Map(r => r.Address).Name("Address");
+        Map(r => r.JoiningDate).Name("JoiningDate").TypeConverterOption.Format("s");
+    }
+}
+
+public class AddressInfo
+{
+    public string Name { get; set; }
+    public string Organisation { get; set; }
+    public string Address { get; set; }
+    public DateTime JoiningDate { get; set; }
+
+
+    public static List<AddressInfo> GetAddress()
+    {
+        return new List<AddressInfo>
+        {
+            new AddressInfo
+            {
+               Name = "Bhavesh",
+                Organisation = "MorningStar",
+                Address = "Mumbai",
+                JoiningDate = new DateTime(2021, 11, 06)
+            }
+            ,
+               new AddressInfo
+            {
+               Name = "Sagar",
+                Organisation = "Accenture",
+                Address = "Delhi",
+                JoiningDate = new DateTime(2021, 11, 04)
+            }
+            ,
+
+               new AddressInfo
+            {
+               Name = "Karan",
+                Organisation = "MorningStar",
+                Address = "Mumbai",
+                JoiningDate = new DateTime(2021, 01, 06)
+            },
+               new AddressInfo
+            {
+               Name = "Ram",
+                Organisation = "Axis",
+                Address = "Nashik",
+                JoiningDate = new DateTime(2020, 11, 06)
+            },
+               new AddressInfo
+            {
+               Name = "Rajesh",
+                Organisation = "MorningStar",
+                Address = "UK",
+                JoiningDate = new DateTime(2018, 11, 06)
+            },
+               new AddressInfo
+            {
+               Name = "seema",
+                Organisation = "ICICI",
+                Address = "Chennai",
+                JoiningDate = new DateTime(2012, 11, 06)
+            },
+               new AddressInfo
+            {
+               Name = "rita",
+                Organisation = "MorningStar",
+                Address = "Mumbai",
+                JoiningDate = new DateTime(2021, 11, 06)
+            }, new AddressInfo
+            {
+               Name = "Bhavesh",
+                Organisation = "MorningStar",
+                Address = "Mumbai",
+                JoiningDate = new DateTime(2021, 11, 06)
+            },
+               new AddressInfo
+            {
+               Name = "Bhavesh",
+                Organisation = "MorningStar",
+                Address = "Mumbai",
+                JoiningDate = new DateTime(2021, 11, 06)
+            }
+
+         ,
+            new AddressInfo
+            {
+                Name = "Phoenix",
+                Organisation = "NASA",
+                 Address = "Mumbai",
+                JoiningDate = new DateTime(2043, 04, 05)
+            }
+        };
+    }
+}
---
+++ FileTocsv/Program.cs
@@ -0,0 +1,25 @@
+﻿
+using System;
+using CsvHelper;
+using System.IO;
+using System.Globalization;
+
+namespace FileTocsv
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var csvPath = Path.Combine(Environment.CurrentDirectory, @"C:\Users\slad\source\repos\FileTocsv\FileTocsv\Utility\Address.csv");
+            using (var streamWriter = new StreamWriter(csvPath))
+            {
+                using (var csvWriter = new CsvWriter(streamWriter, CultureInfo.InvariantCulture))
+                {
+                    var rockets = AddressInfo.GetAddress();
+                    csvWriter.Context.RegisterClassMap<AddressInfoClassMap>();
+                    csvWriter.WriteRecords(rockets);
+                }
+            }
+        }
+    }
+}
